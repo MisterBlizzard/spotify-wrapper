bb0c7023829d480e3248c15072f363525aab6805
misterblizzard <ax-filipe@hotmail.com>
2018-05-23 04:32:44 -0500

    configuring eslint]

--- tests/docs/calc.spec.js
+++ tests/docs/calc.spec.js
@@ -1,36 +1,35 @@
-import { expect } from 'chai'
-import { sum, sub, mult, div } from '../src/main.js'
+import { expect } from "chai";
+import { sum, sub, mult, div } from "../src/main.js";
 
-describe('Calc', () => {
-
-    describe('Smoke tests', () => {
+describe("Calc", () => {
+    describe("Smoke tests", () => {
     })
 
-    describe('Sum', () => {
-        it('should return 4 when `sum(2, 2)`', () => {
+    describe("Sum", () => {
+        it("should return 4 when `sum(2, 2)`", () => {
             expect(sum(2, 2)).to.be.equal(4)
         })
     })
 
-    describe('Sub', () => {
-        it('should return 4 when `sub(6, 2)`', () => {
+    describe("Sub", () => {
+        it("should return 4 when `sub(6, 2)`", () => {
             expect(sub(6, 2)).to.be.equal(4)
         })
     })
 
-    describe('Mult', () => {
-        it('should return 30 when `mult(6, 5)`', () => {
+    describe("Mult", () => {
+        it("should return 30 when `mult(6, 5)`", () => {
             expect(mult(6, 5)).to.be.equal(30)
         })
     })
 
-    describe('Div', () => {
-        it('should return 5 when `div(40, 8)`', () => {
+    describe("Div", () => {
+        it("should return 5 when `div(40, 8)`", () => {
             expect(div(40, 8)).to.be.equal(5)
         })
 
         it('should return "division by zero is not possible" when divide by 0', () => {
-            expect(div(40, 0)).to.be.equal('division by 0 is not possible')
+            expect(div(40, 0)).to.be.equal("division by 0 is not possible")
         })
     })
 })
--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -1,37 +1,37 @@
-var expect = require('chai').expect
+const expect = require('chai').expect;
 
 describe('Class Main', function() {
-    let arr
+    let arr;
 
     // roda todas as vezes, antes de cada bloco
     beforeEach(function() {
-        arr = [1, 2, 3]
-    })
+        arr = [1, 2, 3];
+    });
 
     it('should be an array', function() {
-        expect(arr).to.be.an('array')
-    })
+        expect(arr).to.be.an('array');
+    });
     
     it('should have a size of 4 when push another value to the array', function() {
-        arr.push(4)
-        expect(arr).to.have.lengthOf(4)
-    })
+        arr.push(4);
+        expect(arr).to.have.lengthOf(4);
+    });
 
     it('should have a size of 2 when pop a value from the array', function() {        
-        arr.pop(4)
-        console.log(arr.length)
-    })
+        arr.pop(4);
+        console.log(arr.length);
+    });
 
     it('should remove the last value of the array (3 in this case)', function() {        
-        arr.pop()
-        expect(arr).to.not.include(3)
-    })    
+        arr.pop();
+        expect(arr).to.not.include(3);
+    });    
 
     it('should have a size of 2 when pop a value of the array', function() {          
-        arr.pop()
-        expect(arr).to.have.lengthOf(2)
-    })
-})
+        arr.pop();
+        expect(arr).to.have.lengthOf(2);
+    });
+});
 
 // O bloco acima irá ser executado na sequência:
 
